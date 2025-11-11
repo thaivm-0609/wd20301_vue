22ff01bd61eb081453461556820b722ffc0609e1
thaivm-0609 <vuongthai95@gmail.com>
2025-11-11 18:10:31 +0700

    feat: data binding

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,11 +32,15 @@ const users = reactive([
     name: 'thaivm2',
     avatar: 'https://picsum.photos/200',
     gender: 'Male',
+    age: 67,
+    status: true,
   },
   {
     name: 'thaivm3',
     avatar: 'https://picsum.photos/200',
     gender: 'Female',
+    age: 14,
+    status: false,
   }
 ])
 
@@ -66,16 +70,36 @@ const users = reactive([
           <th>Name</th>
           <th>Avatar</th>
           <th>Gender</th>
+          <th>Age</th>
+          <th>Status</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
+        <!-- v-bind: cú pháp:
+          - đầy đủ: <element v-bind:attribute="value"></element>
+          - rút gọn: <element :attribute="value"></element>
+
+          - class binding: :class="tenClass"
+        -->
         <tr>
           <td>1</td>
           <td>{{ users[0].name }}</td>
-          <td></td>
-          <td>{{ users[0].gender }}</td>
-          <td>
+          <td><img v-bind:src="users[0].avatar" alt=""></td>
+          <!-- cú pháp cho class binding có điều kiện
+            :class="{ tenClass: điều kiện }" 
+          -->
+          <td :class="{ isMale: users[0].gender == 'Male'}">{{ users[0].gender }}</td>
+          <!-- style binding: :style="{
+            tenThuocTinh: 'value' (ko có điều kiện)
+            tenThuocTinh1: điều kiện ? 'giá trị nếu đk đúng' : 'giá trị nếu điều kiện sai'
+          }" -->
+          <td :style="{ fontSize: users[0].age>65 ? '50px' : '20px' }">
+            {{ users[0].age }}
+          </td>
+          <td :class="{ isActive: users[0].status == true }">{{ users[0].status }}</td>
+          <td style="background-color: red;">
+            <button @click="">Change status</button>
             <button class="btn btn-warning">Edit</button>
             <button class="btn btn-danger">Delete</button>
           </td>
@@ -83,8 +107,10 @@ const users = reactive([
         <tr>
           <td>2</td>
           <td>{{ users[1].name }}</td>
-          <td></td>
-          <td>{{ users[1].gender }}</td>
+          <td><img :src="users[1].avatar" alt=""></td>
+          <td :class="{ isMale: users[1].gender == 'Male'}">{{ users[1].gender }}</td>
+          <td>{{ users[1].age }}</td>
+          <td :class="{ isActive: users[1].status == true }">{{ users[1].status }}</td>
           <td>
             <button class="btn btn-warning">Edit</button>
             <button class="btn btn-danger">Delete</button>
@@ -101,6 +127,15 @@ h1 {
   font-size: 2.6rem;
   position: relative;
   top: -10px;
+}
+
+.isMale {
+  background-color: green;
+}
+
+.isActive {
+  background-color: purple;
+  font-size: 40px;
 }
 
 h3 {
